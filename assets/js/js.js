9de5df1951017e7465e50e370265cecd4c3c5ef4
Paulo60650 <paul.dacheux@gmail.com>
2020-02-14 19:47:04 +0100

    premier mise en page

--- assets/js/js.js
+++ assets/js/js.js
@@ -6,17 +6,20 @@ let idres1;
 let idres2;
 let count = 0;
 let temps = 240;
-
 let btnjouer = document.getElementById("jouer");
+let btnrejouer = document.getElementById("recommencer");
+
+btnrejouer.addEventListener('click', function ()
+   {
+    window.location.reload();
+});  
 
 btnjouer.addEventListener('click', function ()
    {
     jouer();
     cmptrb();
     
-}     
-        );  
-
+});  
 
 function melange(tab)
 {
@@ -31,13 +34,11 @@ function jouer()
 
     for (let i = 1; i <= tabm.length; i++)
     {
-
         let name = document.getElementById('img' + i.toString());
         name.addEventListener('click', function ()
         {
             if (res1 == undefined || res2 == undefined)
             {
-
                 {
                     name.style.backgroundImage = "url('assets/img/" + tabm[i - 1].toString() + ".png')";
 
@@ -45,46 +46,28 @@ function jouer()
                     {
                         res1 = tabm[i - 1];
                         idres1 = name.id.toString();
-
-
                     } else
                     {
                         if (res2 == undefined)
                             res2 = tabm[i - 1];
                         idres2 = name.id.toString();
-
-
-
                     }
                     if (idres1 == idres2)
                     {
                         res2 = undefined;
                     }
-
                 }
             }
-
             if ((res1 != undefined && res2 != undefined) && (res1 !== res2))
-
-
             {
 
                 setTimeout(function () {
-
-
                     document.getElementById(idres1).style.backgroundImage = "url('assets/img/spritesheet.png')";
                     document.getElementById(idres2).style.backgroundImage = "url('assets/img/spritesheet.png')";
                     res2 = undefined;
                     res1 = undefined;
-
-
                 }, 1000);
-
-
-
             }
-
-
             if ((res1 != undefined && res2 != undefined) && (res1 == res2) && (idres1 != idres2)) {
 
                 document.getElementById(idres1).removeAttribute("onclick");
@@ -95,25 +78,13 @@ function jouer()
                 res1 = undefined;
 
             }
-
-
-
         });
-
-
-
-
     }
 }
-
-
-// name.addEventListener('click',function(){});
 
 function shuffle(array) {
     array.sort(() => Math.random() - 0.5);
 }
-
-
 
 function cmptrb()
 {
@@ -122,7 +93,7 @@ function cmptrb()
     let min = 0;
     if (sec < 0)
     {
-        if (confirm("Le temps est écoulé !!! \n Recommencer ?") == true) {
+        if (confirm("Fin du temps !!! \n Recommencer ?") == true) {
             window.location.reload();
         } else {
             window.close();
